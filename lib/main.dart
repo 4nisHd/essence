a8a4e6ab9691260c5ae39bd5a9850bc5752955fb
Anisx-D <105158553+Anisx-D@users.noreply.github.com>
2022-05-08 07:02:30 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter App',
+      title: '',
       home: MyHomePage(),
     );
   }
